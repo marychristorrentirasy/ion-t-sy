b672dbaed0e7fa5f469de21b3f59b1e86659f2cb
marychristorrentirasy <marychristorrentirasy@gmail.com>
2024-05-07 09:15:33 +0800

    Updates

--- assets/md.transition-legacy-vgvmh5xn.js
+++ assets/md.transition-legacy-vgvmh5xn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-Co8QQuF8.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-BHK7FcG-.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */

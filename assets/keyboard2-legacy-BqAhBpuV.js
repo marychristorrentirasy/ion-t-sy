2b433972b4d5a14efc7cfa2f245302caccbe48a1
marychristorrentirasy <marychristorrentirasy@gmail.com>
2024-05-10 09:37:31 +0800

    Updates

--- assets/keyboard2-legacy-BqAhBpuV.js
+++ assets/keyboard2-legacy-BqAhBpuV.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BHK7FcG-.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-DCpSFa5G.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
